8602830505acc98f3b30cdc06c09c86af170f4f3
Punnoose K Thomas <punnoose@dexlock.com>
2024-12-30 00:38:22 +0100

    Updated Query Tester

--- queryTester.py
+++ queryTester.py
@@ -1,11 +1,43 @@
 # To run with log, use the below command
 # python .\queryTester.py > .\outputLogs\queryTestOutput.txt
 
+import json
 import logging
 from tqdm import tqdm
 from queries import *
 from constants import *
 from rdflib import Graph
+
+def resultsToJson(query_results):
+    """
+    Converts SPARQL query results into a list of JSON objects.
+    
+    :param query_results: SPARQL query results from rdflib's `Graph.query` method.
+    :return: List of JSON objects with predicate as keys and object as values.
+    """
+    json_objects = []
+
+    # Iterate through the SPARQL results
+    for result in query_results:
+        # Create a dictionary for each result
+        json_obj = {}
+        for var in result.labels.keys():
+            value = result[var]
+            if value is not None:
+                json_obj[var] = str(value)  # Convert RDF terms to strings
+
+        json_objects.append(json_obj)
+        print("Converted Data Sample : ",json.dumps(json_objects[0], indent=4))
+    return json_objects
+
+def checkKeyValue(queryResults, key:str, value:str):
+    existsCount = 0
+    if len(queryResults)>0:
+        for item in tqdm(queryResults, desc="Checking response queries...."):
+            tempValue = str(item[key])
+            if value in tempValue:
+                existsCount = existsCount+1
+    print(f"Result Length : Length of items with Value in Key\n{len(queryResults)}          : {existsCount}")
 
 def query_rdf_file(rdf_file, sparql_query):
     """
@@ -32,8 +64,11 @@ def query_rdf_file(rdf_file, sparql_query):
     
     # Print results
     print("Results size : ",len(results))
-    for result in tqdm(results, desc="Parsing Results...."):
-        print(result['title'])
+    # for result in tqdm(results, desc="Parsing Results...."):
+        # print(result)
+    if len(results)>0:
+        checkKeyValue("title", "Andrew Tiernan")
+        jsonData = resultsToJson(results)
     print("Query executed successfully.")
 
 if __name__=="__main__":
